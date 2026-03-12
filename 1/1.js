ffb2d6295514404ba41ad682d75347ddc0833296
RealRusLink <real.rus.link@gmail.com>
2026-03-12 13:55:38 +0200

    1 is a lib

--- 1/1.js
+++ 1/1.js
@@ -1,16 +1,12 @@
-function* iterate(list){
+export function* iterate(list){
     while(true){
         yield* list
     }
 }
 
-function consume(time, iterator){
+export function consume(time, iterator){
     const start = performance.now()
     while (performance.now() - start < time*1000){
         console.log(iterator.next().value)
     }
 }
-
-
-let a = iterate([1, 2, 3, 4])
-consume(3, a)

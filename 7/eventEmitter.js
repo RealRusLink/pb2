1acca141bf79ab422f9d5bb727e67c1aaa306455
RealRusLink <real.rus.link@gmail.com>
2026-05-08 14:33:57 +0300

    movement detector functionality

--- 7/eventEmitter.js
+++ 7/eventEmitter.js
@@ -1,5 +1,8 @@
 import {EventEmitter} from "node:events"
 
 class MovementDetector extends EventEmitter {
-    
+    detect(x, y){
+        console.log(`Detected movement at (${x}, ${y})`)
+        this.emit("movement", {x, y})
+    }
 }
